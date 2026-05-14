2794c1daa01ea8256e2ff04c18f4e1a4f36ca108
bernardomiller <202407095798@alunos.ibmec.edu.br>
2026-05-14 12:44:22 -0300

    fix: filtros de busca por matricula, nome e cpf

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Aluno
 from .serializers import AlunoSerializer
 
 class AlunoListView(generics.ListAPIView):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['matricula', 'nome', 'cpf']
