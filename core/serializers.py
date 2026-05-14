2389bd438296fee987280da36cd8fbe427ec600c
Otto <ottobgoldner@gmail.com>
2026-05-14 16:51:11 -0300

    Serializer do Aluno

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = ["nome","areaId"]
+
+class AlunoSerializer(serializers.ModelSerializer):
+    nome = serializers.CharField(max_length=100)
+    matricula = serializers.CharField(max_length=20)
+    cpf = serializers.CharField(max_length=14)
+    is_ativo = serializers.BooleanField()
