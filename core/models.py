313e220eff44423412e6012ad04aeb7e33718d91
bernardomiller <202407095798@alunos.ibmec.edu.br>
2026-05-14 03:10:28 -0300

    inseri modelos de  Curso e Area (Issue 15.4)

--- core/models.py
+++ core/models.py
@@ -13,3 +13,27 @@ class Aluno(models.Model):
     def __str__(self): ##
         # Isso define como o aluno vai aparecer no painel da Secretaria (ex: "dr. bazinga - 20236769420")
         return f"{self.nome} - {self.matricula}"
+
+
+class Area(models.Model):
+    nome = models.CharField(max_length=100, unique=True, verbose_name="Nome da Área")
+    descricao = models.TextField(blank=True, null=True, verbose_name="Descrição")
+
+    class Meta:
+        verbose_name = "Área"
+        verbose_name_plural = "Áreas"
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=150, verbose_name="Nome do Curso")
+    area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True, related_name='cursos', verbose_name="Área")
+    is_ativo = models.BooleanField(default=True, verbose_name="Status Ativo")
+
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+
+    def __str__(self):
+        return self.nome
