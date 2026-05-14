dcbfb5687be0b97f3f164920117f0f25d55b5edf
Otto <ottobgoldner@gmail.com>
2026-05-14 17:13:52 -0300

    Singleobj melhorado + Multipleobj e json

--- core/views.py
+++ core/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import *
 from .serializers import AlunoSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
 def singleobj(request):
     data = Aluno.objects.get(id=1)
     serializer = AlunoSerializer(data)
-    print(serializer.data)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
 
+def multipleobj(request):
+    data = Aluno.objects.all()
+    serializer = AlunoSerializer(data, many=True)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
