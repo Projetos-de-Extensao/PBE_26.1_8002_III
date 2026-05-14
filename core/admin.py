bb6c8d23c6796b7954da130bf05dc6e1ab746d19
Otto <ottobgoldner@gmail.com>
2026-05-14 16:11:41 -0300

    Adicionar Aluno aos users no Admin

--- core/admin.py
+++ core/admin.py
@@ -6,3 +6,8 @@ admin.site.register(Contrato)
 admin.site.register(Processo)
 admin.site.register(Area)
 admin.site.register(Curso)
+
+# Conseguimos adicionar os alunos diretamente aos usuarios do Django Admin
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'matricula', 'cpf', 'is_ativo')
