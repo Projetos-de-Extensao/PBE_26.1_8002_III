23746c818f2e4dc5a092ccaa996b9bd0adea27ea
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-05-15 17:25:11 -0300

    Revert "feat endpoint GET /alunos (Issue 23)"

--- core/views.py
+++ core/views.py
@@ -1,9 +1,3 @@
-from rest_framework import generics, filters
-from .models import Aluno
-from .serializers import AlunoSerializer
+from django.shortcuts import render
 
-class AlunoListView(generics.ListAPIView):
-    queryset = Aluno.objects.all()
-    serializer_class = AlunoSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['matricula', 'nome', 'cpf']
+# Create your views here.
