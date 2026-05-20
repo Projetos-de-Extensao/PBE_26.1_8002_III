eab65b0801e2d9ce85332ead8b66043aff135ec5
Otto <ottobgoldner@gmail.com>
2026-05-20 13:48:05 -0300

    Bug correction

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ import io
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from dnjango.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -17,7 +17,7 @@ from rest_framework.authentication import TokenAuthentication
 
 @csrf_exempt
 def aluno(request,id):
-    data = Aluno.objects.get(id=id)
+    data = get_object_or_404(Aluno,id=id)
 
     if request.method== 'PUT':
         stream = io.BytesIO(request.body)
