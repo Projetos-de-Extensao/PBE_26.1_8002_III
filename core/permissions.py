9a45d27112084d67bffe4a43f10dd40a2e6de447
Otto <ottobgoldner@gmail.com>
2026-05-20 15:17:19 -0300

    Aluno

--- core/permissions.py
+++ core/permissions.py
@@ -1,3 +1,9 @@
 from rest_framework.permissions import BasePermission
 from .models import Aluno, Secretaria, Coordenador
 
+class IsAluno(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+        return Aluno.objects.filter(email=request.user.email).exists()
+
