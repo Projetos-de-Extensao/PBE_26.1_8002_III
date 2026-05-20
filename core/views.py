4512f8b5e4432786ef276c0b278b81a5c2b11593
Otto <ottobgoldner@gmail.com>
2026-05-20 15:21:20 -0300

    Imports 2

--- core/views.py
+++ core/views.py
@@ -7,6 +7,9 @@ import io
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from .permissions import IsSecretaria, IsAluno
 
 @csrf_exempt
 def aluno(request):
