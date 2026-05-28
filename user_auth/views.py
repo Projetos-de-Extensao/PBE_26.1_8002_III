eae4c3874822d6a4e9ea6ea290dc56d7f3a1c3ee
Otto <ottobgoldner@gmail.com>
2026-05-28 17:12:19 -0300

    Adicionando redefiniçao de senha

--- user_auth/views.py
+++ user_auth/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.core.cache import cache
 from rest_framework.request import Request
+import logging
+from django.contrib.auth.hashers import make_password
 
 from core.models import Aluno
 
@@ -46,6 +48,11 @@ class LoginAPIView(APIView):
                         {"message": "Conta desativada. Por favor, entre em contato com a secretaria."}, 
                         status=status.HTTP_403_FORBIDDEN
                     )
+                if aluno.precisa_redefinir_senha:
+                    return Response(
+                        {"message": "Você precisa redefinir sua senha antes de continuar."}, 
+                        status=status.HTTP_403_FORBIDDEN
+                    )
             except Aluno.DoesNotExist:
                 pass
         
@@ -60,3 +67,41 @@ class LoginAPIView(APIView):
                 {"message": f"Credenciais inválidas. Tentativa {attempts}/5."}, 
                 status=status.HTTP_401_UNAUTHORIZED
             )
+
+
+logger = logging.getLogger(__name__)
+
+class PrimeiroAcessoAPIView(APIView):
+    
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request: Request):
+        usuario = request.data.get('username')
+        senha_atual = request.data.get('old_password')
+        nova_senha = request.data.get('new_password')
+
+        user = authenticate(request, username=usuario, password=senha_atual)
+        
+        if user is not None:
+            try:
+                aluno = Aluno.objects.get(matricula=usuario)
+                
+                if not aluno.precisa_redefinir_senha:
+                    return Response({"message": "Sua conta já realizou o primeiro acesso."}, status=status.HTTP_400_BAD_REQUEST)
+
+                aluno.senha = make_password(nova_senha)
+                aluno.precisa_redefinir_senha = False
+                aluno.save()
+
+                user.set_password(nova_senha)
+                user.save()
+
+                logger.info(f"[SEGURANÇA] O aluno com matrícula {usuario} concluiu a redefinição de senha do primeiro acesso.")
+
+                return Response({"message": "Senha redefinida com sucesso! Agora você pode fazer o login normalmente."}, status=status.HTTP_200_OK)
+
+            except Aluno.DoesNotExist:
+                return Response({"message": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({"message": "Credenciais atuais inválidas."}, status=status.HTTP_401_UNAUTHORIZED)
