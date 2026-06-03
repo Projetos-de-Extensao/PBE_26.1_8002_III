061dd94b5ce03a2bf0ef31824dcaff538e135dd3
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-06-03 14:16:35 -0300

    feat: add service functions for dynamic file path generation and PDF validation utility

--- core/services.py
+++ core/services.py
@@ -4,6 +4,7 @@ import uuid
 from django.core.exceptions import ValidationError
 
 def upload_contrato_path(instance,matricula,filename):
+    matricula = instance.processoId.matricula_aluno.matricula
     tipo = filename.split('.')[-1]
     nome_novo = f"{uuid.uuid4()}.{tipo}"
     return os.path.join(matricula,"contratos",nome_novo)
