1f516acdf0a2ba0e8be048e19d061dc229f20ace
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-06-04 18:56:30 -0300

    fix: sintax errors on services and settings

--- core/services/upload_contrato.py
+++ core/services/upload_contrato.py
@@ -2,7 +2,7 @@ import os
 import uuid
 
 def upload_contrato_path(instance, filename):
-    matricula = instance.processoId.matricula_aluno.matricula
+    matricula = instance.processoId.aluno.matricula
     tipo = filename.split('.')[-1]
     nome_novo = f"{uuid.uuid4()}.{tipo}"
     return os.path.join(matricula, "contratos", nome_novo)
