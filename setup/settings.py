542b721a24b8649d43817c87f7a7cb62cc1f18b1
Otto <ottobgoldner@gmail.com>
2026-06-03 22:19:32 -0300

    Implementando test Mailtrap

--- setup/settings.py
+++ setup/settings.py
@@ -1,5 +1,9 @@
+import os
 from pathlib import Path
-from datetime import timedelta
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +134,12 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'sandbox.smtp.mailtrap.io')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 2525))
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER', '') 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', '') 
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = 'notificacoes@ibintern.com.br'
