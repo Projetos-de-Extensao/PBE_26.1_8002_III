753580924c443f70d431e9f2dc81fed1bafe5dd5
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-06-08 23:07:19 -0300

    feat: classe de erro personalizada para facilitar validação

---
+++ core/exceptions.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+class gradeHorariaIncompativelException(Exception):
+    def __init__(self, message="Grade horária incompatível com o contrato!"):
+        self.message = message
+        super().__init__(self.message)
