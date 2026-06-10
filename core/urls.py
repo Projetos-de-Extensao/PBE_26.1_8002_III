86d14b776fe625970f0cb1e179d4bf05b1f1d8b2
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-06-10 12:07:29 -0300

    feat(core): implement new views and URL endpoints for contract download, reprovação, and notifications

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import UploadContrato, AvaliarContratoAPIView, AvaliarRelatorioAPIView, UploadRelatorio, ReprovarContratoAPIView
+from .views import UploadContrato, AvaliarContratoAPIView, AvaliarRelatorioAPIView, UploadRelatorio, ReprovarContratoAPIView, DownloadContratoAPIView
 
 urlpatterns = [
     path('aluno/', views.AlunoAPIView.as_view()),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('processo/<int:id>/', views.ProcessoDetailAPIView.as_view()),
     path('processo/<int:id>/contrato/', UploadContrato.as_view(), name='upload_contrato'),
     path('contrato/avaliar/', AvaliarContratoAPIView.as_view(), name='avaliar_contrato'),
+    path('contrato/<int:id>/download/', DownloadContratoAPIView.as_view(), name='download_contrato'),
     path('processo/<int:id>/reprovar/', ReprovarContratoAPIView.as_view(), name='reprovar_contrato'),
     path('processo/<int:id>/relatorio/', UploadRelatorio.as_view(), name='upload_relatorio'),
     path('relatorio/avaliar/', AvaliarRelatorioAPIView.as_view(), name='avaliar_relatorio'),
